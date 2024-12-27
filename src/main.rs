0dd453acfb30867e641cec67106c329461c5ac17
Erik <ebobinski0@gmail.com>
2024-12-26 20:46:49 -0500

    reslotting functionality - ui subdirs and config.json still need to be reslotted

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
-// mod mod_directory;
-
-// use mod_directory::*;
 use native_dialog::FileDialog;
-use std::{
-    fs::{self, File},
-    io::Write,
-    process,
-};
+use regex::Regex;
+use std::{fs::rename, io::Write, path::PathBuf, process};
 use walkdir::WalkDir;
 
 fn main() {
+    let (c_slot, root) = gather_inputs();
+    reslotter(c_slot, root);
+}
+
+// gather and validate proper mod path and c-slot
+fn gather_inputs() -> (String, String) {
     // take user-input of costume slot
-    let c_slot = loop {
+    let c_slot: String = loop {
         // run the input
         let mut input = String::new();
         print!("Enter the costume slot to reslot to (example: for c00, enter 00): c");
@@ -38,11 +38,13 @@ fn main() {
             println!("Only enter two integers for the slot!\n")
         }
     };
+    // confirm c-slot target
     println!("\x1b[32m✔\x1b[0m Costume slot read as c{}", c_slot);
 
     // take user-input of mod path
-    let root_path = loop {
-        let root = FileDialog::new().set_location("~").show_open_single_dir();
+    let root: PathBuf = loop {
+        let root: Result<Option<PathBuf>, native_dialog::Error> =
+            FileDialog::new().set_location("~").show_open_single_dir();
 
         match root {
             Ok(Some(path)) => {
@@ -57,31 +59,55 @@ fn main() {
             }
         }
     };
-    // convert path to String in unix-style
-    let root_str = root_path.to_string_lossy().to_string().replace("\\", "/");
-    println!("\x1b[32m✔\x1b[0m Mod path read as: {:?}", root_str);
+    // confirm path in unix-style
+    let root: String = root.to_string_lossy().to_string();
+    println!(
+        "\x1b[32m✔\x1b[0m Mod path read as: {:?}",
+        root.replace("\\", "/")
+    );
 
-    // TODO: cXY pattern identification and renaming
-
-    // rename example.txt to example_renamed.txt
-    // let res: Result<(), std::io::Error> =
-    //     fs::rename("./example/example.txt", "./example/example_renamed.txt");
-    // if let Err(e) = &res {
-    //     println!("error occurred: {}", e);
-    // }
-
-    // println!("calling print_directory on {}:", target_dir);
-    // let res = print_directory(&target_dir);
-    // println!("{:?}", res);
-    // println!("------------------------------------------------------------");
+    (c_slot, root)
 }
 
-// print every entry in a root directory
-fn print_directory(target_dir: &str) {
-    for entry in WalkDir::new(target_dir) {
-        match entry {
-            Ok(e) => println!("Found: {}", e.path().display()),
-            Err(e) => println!("Error reading entry: {}", e),
+// parse mod file for c-slots and replace them
+fn reslotter(c_slot: String, root: String) {
+    let c_pattern = Regex::new(r"c\d\d").unwrap();
+
+    let entries: Vec<_> = WalkDir::new(&root)
+        .into_iter()
+        .filter_map(|entry| entry.ok())
+        .collect();
+
+    // sort entries by decreasing depth
+    let mut sorted_entires: Vec<walkdir::DirEntry> = entries;
+    sorted_entires.sort_by(|a, b| b.depth().cmp(&a.depth()));
+
+    for entry in sorted_entires {
+        let file_name = entry.file_name().to_string_lossy().to_string();
+
+        // detect cXY pattern
+        if c_pattern.is_match(&file_name) {
+            let new_file_name = c_pattern.replace(&file_name, format!("c{}", c_slot));
+
+            let current_path = entry.path();
+
+            let new_path = current_path
+                .parent()
+                .unwrap()
+                .join(new_file_name.to_string());
+
+            if let Err(e) = rename(current_path, &new_path) {
+                eprintln!(
+                    "Failed to rename {:?} to {:?}, {}",
+                    current_path, new_path, e
+                );
+            } else {
+                println!(
+                    "\x1b[32m✔\x1b[0m Renamed {:?} to {:?}",
+                    current_path.file_name().unwrap(),
+                    new_path.file_name().unwrap()
+                );
+            }
         }
     }
 }
